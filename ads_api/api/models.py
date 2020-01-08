c22fb5266e5bb5c2ae9ab51f5240be4a54a057a5
sid <milosida@gmail.com>
2020-01-08 18:18:24 +0300

    now you add ad via POST

--- ads_api/api/models.py
+++ ads_api/api/models.py
@@ -7,6 +7,9 @@ class Ad(models.Model):
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["created_at"]
+
     def __str__(self):
         return self.title
 
@@ -14,7 +17,7 @@ class Ad(models.Model):
 class Photo(models.Model):
     url = models.CharField(max_length=300)
     upload_at = models.DateTimeField(auto_now_add=True)
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.url
--- ads_api/api/serializers.py
+++ ads_api/api/serializers.py
@@ -3,16 +3,37 @@ from rest_framework import serializers
 from .models import Ad, Photo
 
 
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ("url",)
+
+
 class AdSerializer(serializers.ModelSerializer):
-    main_photo = serializers.SerializerMethodField("get_main_photo")
+    photos = PhotoSerializer(many=True)
 
     class Meta:
         model = Ad
-        fields = ("title", "price", "main_photo")
+        fields = ("title", "description", "price", "photos")
 
-    def get_main_photo(self, ad):
-        photo = Photo.objects.filter(ad=ad).first()
-        if photo:
-            return photo.url
-        else:
-            return 0
+    def create(self, validated_data):
+        photos_data = validated_data.pop("photos")
+        ad = Ad.objects.create(**validated_data)
+        photos_objects = [Photo(ad=ad, url=photo["url"]) for photo in photos_data]
+        Photo.objects.bulk_create(photos_objects)
+        return ad
+
+
+# class AdSerializer(serializers.ModelSerializer):
+#     main_photo = serializers.SerializerMethodField("get_main_photo")
+
+#     class Meta:
+#         model = Ad
+#         fields = ("title", "price", "main_photo")
+
+#     def get_main_photo(self, ad):
+#         photo = Photo.objects.filter(ad=ad).first()
+#         if photo:
+#             return photo.url
+#         else:
+#             return 0
