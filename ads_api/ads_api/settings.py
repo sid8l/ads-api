dc99e8fbd21a10a76667790485d02e07f3a60755
Novikov Artem <milosida@gmail.com>
2020-01-14 19:34:29 +0300

    change db to postgresql

--- ads_api/ads_api/settings.py
+++ ads_api/ads_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "ads_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ads_db",
+        "USER": "ads_user",
+        "PASSWORD": "ads_password",
+        "HOST": "localhost",
+        "PORT": "", 
     }
 }
 
